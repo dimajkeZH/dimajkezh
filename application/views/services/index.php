c23f2a78f92c00d96a3bdfe5e444d29be1eddd47
dimajkezh <dimajkezh@gmail.com>
2018-03-25 20:11:29 +0300

    services

--- application/views/services/index.php
+++ application/views/services/index.php
@@ -1,2 +1,65 @@
-Услуги
-<?php echo $param; ?>
+	<div class="main_wrapper">
+		<div class="services_head_wrapper">
+			<div class="services_head">
+				<div class="services_head_info">
+					<img src="/assets/img/services/services_head.png" alt="">
+					<div class="services_head_text">
+						<p>Услуги</p>
+						<p>Выберите любую услугу из перечня и сделайте заказ автобуса в нашей компании. Наши опытные диспетчера и профессиональные водители будут рады ждут ваших заказов в любое время суток. Компания "ТриБас-М" предоставляет надежный парк автобусов. Круглосуточная работа диспетчерской, обеспечивает высокое качество нашего сервиса, оказания услуг по перевозке пассажиров в городе Москве и Московской области.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="services_list_wrapper">
+			<div class="services_list">
+				<div class="services_list_title">
+					<p>Список услуг</p>
+					 <div class="services_line"></div>
+				</div>
+				<div class="services_list_items">
+					<div class="services_list_item">
+						<p>Автобус в аэропорт</p>
+						<img src="/assets/img/services/air.png" alt="">
+						<div class="services_list_button">
+							<a href="#"><p>Подробнее</p></a>
+						</div>
+					</div>
+					<div class="services_list_item">
+						<p>Автобус на свадьбу</p>
+						<img src="/assets/img/services/wedding.png" alt="">
+						<div class="services_list_button">
+							<a href="#"><p>Подробнее</p></a>
+						</div>
+					</div>
+					<div class="services_list_item">
+						<p>Автобус межгород</p>
+						<img src="/assets/img/services/sities.png" alt="">
+						<div class="services_list_button">
+							<a href="#"><p>Подробнее</p></a>
+						</div>
+					</div>
+					<div class="services_list_item">
+						<p>Список документов для заказных автобусов</p>
+						<img src="/assets/img/services/docs.png" alt="">
+						<div class="services_list_button">
+							<a href="#"><p>Подробнее</p></a>
+						</div>
+					</div>
+					<div class="services_list_item">
+						<p>Срочный заказ автобуса</p>
+						<img src="/assets/img/services/fast.png" alt="">
+						<div class="services_list_button">
+							<a href="#"><p>Подробнее</p></a>
+						</div>
+					</div>
+					<div class="services_list_item">
+						<p>Поездка до границы Украины и обратно</p>
+						<img src="/assets/img/services/ukraine.png" alt="">
+						<div class="services_list_button">
+							<a href="#"><p>Подробнее</p></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
