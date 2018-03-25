dd7bf2fe6b7d1bc9eea80b1434855cbb534d51a4
zh <dimajkezh@gmail.com>
2018-03-25 17:12:00 +0300

    zh9

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
 	<title><?php echo $title; ?></title>
-	<script src="/public/scripts/jquery.js"></script>
-	<script src="/public/scripts/form.js"></script>
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-	<?php echo $content; ?>
+	<div class="header_wrapper">
+		<div class="header">
+			<div class="header_nav">
+				<span class="logo">
+					<a href="/">
+						<img src="assets/img/logo.png" alt="">
+					</a>
+				</span>
+				<div class="main_nav">
+					<nav class="nav">
+						<a href="/services" class="nav_item">Услуги</a>
+						<a href="/buses" class="nav_item">Автобусы</a>
+						<a href="/minivans" class="nav_item">Микроавтобусы</a>
+						<a href="/excursions" class="nav_item">Экскурсии</a>
+						<a href="/contacts" class="nav_item">Контакты</a>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="div"><?php echo $content; ?></div>
+	<div class="footer_wrapper">
+		<div class="footer">
+			<p>© Copyright, 2015 ТК "ТриБас" - Все права защищены.
+			</p>
+			<p>
+			При использовании материалов с сайта ссылка на источник обязательна.
+			</p>
+		</div>
+	</div>
 </body>
 </html>
