57d28f15435ef95def102df5cb54fa9a87ede49f
zh <dimajkezh@gmail.com>
2018-03-26 18:07:34 +0300

    zh13

--- application/models/Services.php
+++ application/models/Services.php
@@ -7,7 +7,7 @@ use application\core\Model;
 class Services extends Model {
 
 	public function getData($param) {
-		$result = [];
+		$result = ['param' => $param];
 		//$result['layout_type'] = $this->db->row('SELEC * FROM kek;',[0=>123,1=>321]);
 		return $result;
 	}
