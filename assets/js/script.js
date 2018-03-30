0362530d97ec42cb823924483dc1892768ca1080
zh <dimajkezh@gmail.com>
2018-03-31 01:17:19 +0300

    zh27

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,16 +30,17 @@ $('#tabs').tabs();
 
 function ResizeWindow(){
 	//* RESIZE vacancies *//
-	var item_info = $(".vacancies_info_text").height() ,
-	item_img = $(".vacancies_info_item_img").height(); 
+	var item_info = $(".vacancies_info_text").outerHeight(),
+	item_img = $(".vacancies_info_item_img").outerHeight(); 
 	var max = Math.max(item_info,item_img); 
 	$(".vacancies_info_item").height(max) ; 
+	//console.log("info_h" + item_info + " img_h: " + item_img + " max:" + max);
 	//* RESIZE images_text *//
-	var item_info = $(".images_text_item_info").height(), 
-	item_img = $(".images_text_item_img").height(); 
+	var item_info = $(".images_text_item_info").outerHeight(), 
+	item_img = $(".images_text_item_img").outerHeight(); 
 	var max = Math.max(item_info,item_img); 
 	$(".images_text_item").height(max); 
-	console.log("info_h" + item_info + " img_h: " + item_img + " max:" + max);
+	//console.log("info_h" + item_info + " img_h: " + item_img + " max:" + max);
 }
 $(window).resize(ResizeWindow);
 $(function() { 
