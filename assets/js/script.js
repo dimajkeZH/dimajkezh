d8b8d655eb47e5f55741b7edcdffd3b4b9471923
dimajkezh <dimajkezh@gmail.com>
2018-03-31 04:48:35 +0300

    dimajke10

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,3 +48,31 @@ $(function() {
 });
 
 
+$(document).ready(function(){
+  $('.main_courusel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+});
