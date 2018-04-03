bfb76b8c0cb0a4dd25617a639857ead399a10297
zh <dimajkezh@gmail.com>
2018-04-03 19:57:01 +0300

    zh_top

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,14 +32,16 @@ $('#tabs').tabs();
 /* изменение высоты родительского блока в зависимости от absolute ребёнка */
 function ResizeWindow(){
 	/* RESIZE vacancies */
-	var item_info = $(".vacancies_info_text").outerHeight(),
-	item_img = $(".vacancies_info_item_img").outerHeight(); 
+	var item_info = $(".vacancies_info_text"),
+		item_info = item_info.outerHeight() + item_info.parent().outerHeight() / 5,
+		item_img = $(".vacancies_info_item_img").outerHeight();
 	var max = Math.max(item_info,item_img); 
-	$(".vacancies_info_item").height(max) ; 
+	$(".vacancies_info_item").height(max); 
 	/*console.log("info_h" + item_info + " img_h: " + item_img + " max:" + max);*/
 	/* RESIZE images_text */
-	var item_info = $(".images_text_item_info").outerHeight(), 
-	item_img = $(".images_text_item_img").outerHeight(); 
+	var item_info = $(".images_text_item_info").outerHeight(),
+		item_info = item_info.outerHeight() + item_info.parent().outerHeight() / 5,
+		item_img = $(".images_text_item_img").outerHeight(); 
 	var max = Math.max(item_info,item_img); 
 	$(".images_text_item").height(max); 
 	/*console.log("info_h" + item_info + " img_h: " + item_img + " max:" + max);*/
