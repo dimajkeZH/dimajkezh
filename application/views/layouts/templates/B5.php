0f5b42ef97a5952532dbf9523c64d5201d45a8ce
zh <dimajkezh@gmail.com>
2018-04-05 03:15:24 +0300

    zh_preBlocks

--- application/views/layouts/templates/B5.php
+++ application/views/layouts/templates/B5.php
@@ -1,8 +1,67 @@
-<br><br><br><br><br><br>
-<br><br><br><br><br><br>
-<h1><center>
-b5
-</center></h1>
-<br><br><br><br><br><br>
-<br><br><br><br><br><br>
-<br><br><br><br><br><br>
+		<div class="catalog_wrapper">
+			<p class="catalog_title">Туристические и междугородние автобусы из Европы и Восточной Азии.</p>
+			<div class="catalog_items">
+				<div class="catalog_item">
+					<div class="catalog_item_text">
+						<p class="catalog_item_title">Марки автобусов из Германии</p>
+						<ul>
+							<li class="catalog_item_link"><a href="">NEOPLAN </a></li>
+							<li class="catalog_item_link"><a href="">MAN</a></li>
+							<li class="catalog_item_link"><a href="">MERCEDES-BENZ TOURIZMO 350 98-07 г</a></li>
+						</ul>
+					</div>
+					<div class="catalog_item_img"><img src="/assets/img/buses/de.png" alt=""></div>
+				</div>
+				<div class="catalog_item">
+					<div class="catalog_item_text">
+						<p class="catalog_item_title">Марки автобусов из Швеции</p>
+						<ul>
+							<li class="catalog_item_link"><a href="">VOLVO</a></li>
+							<li class="catalog_item_link"><a href="">SCANIA</a></li>
+						</ul>
+					</div>
+					<div class="catalog_item_img"><img src="/assets/img/buses/sweden.png" alt=""></div>
+				</div>
+				<div class="catalog_item">
+					<div class="catalog_item_text">
+						<p class="catalog_item_title">Марки автобусов из Италии</p>
+						<ul>
+							<li class="catalog_item_link"><a href="">IVECO</a></li>
+							<li class="catalog_item_link"><a href="">DE SIMON</a></li>
+						</ul>
+					</div>
+					<div class="catalog_item_img"><img src="/assets/img/buses/pizza.png" alt=""></div>
+				</div>
+				<div class="catalog_item">
+					<div class="catalog_item_text">
+						<p class="catalog_item_title">Марки автобусов из Южной Кореи</p>
+						<ul>
+							<li class="catalog_item_link"><a href="">HUYNDAI COUNTY</a></li>
+							<li class="catalog_item_link"><a href="">KIA</a></li>
+						</ul>
+					</div>
+					<div class="catalog_item_img"><img src="/assets/img/buses/korea.png" alt=""></div>
+				</div>
+				<div class="catalog_item">
+					<div class="catalog_item_text">
+						<p class="catalog_item_title">Марки автобусов из Китая</p>
+						<ul>
+							<li class="catalog_item_link"><a href="">HIGER</a></li>
+							<li class="catalog_item_link"><a href="">GOLDEN DRAGON</a></li>
+						</ul>
+					</div>
+					<div class="catalog_item_img"><img src="/assets/img/buses/china.png" alt=""></div>
+				</div>
+				<div class="catalog_item">
+					<div class="catalog_item_text">
+						<p class="catalog_item_title">Марки автобусов из России</p>
+						<ul>
+							<li class="catalog_item_link"><a href="">ЛиАЗ</a></li>
+							<li class="catalog_item_link"><a href="">ГАЗ</a></li>
+							<li class="catalog_item_link"><a href="">ПАЗ</a></li>
+						</ul>
+					</div>
+					<div class="catalog_item_img"><img src="/assets/img/buses/velikaya_derzhava.png" alt=""></div>
+				</div>
+			</div>
+		</div>
