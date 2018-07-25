c330e8cda4fc66baf5156e7d028ff7da84f95602
zh <dimajkezh@gmail.com>
2018-07-25 16:51:12 +0300

    zh

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -6,6 +6,7 @@
 	<meta name="Description" content="<?php echo $HTML_DESCR; ?>">
 	<meta name="Keywords" content="<?php echo $HTML_KEYWORDS; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="format-detection" content="telephone=no">
 	<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 	<link rel="stylesheet" type="text/css" href="/assets/js/slick/slick.css">
 	<link rel="stylesheet" type="text/css" href="/assets/js/slick/slick-theme.css">
